3c1806dba530d6c0a5e11c3539162f9f8a18f073
hachidaime <cold.kid40@gmail.com>
2025-07-30 01:50:10 +0700

    plugins: update keybinds for toggle outline

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -37,7 +37,7 @@ return {
 		lazy = true,
 		cmd = { "Outline", "OutlineOpen" },
 		keys = { -- Example mapping to toggle outline
-			{ "<leader>uo", "<cmd>Outline<CR>", desc = "Toggle outline" },
+			{ "<leader>o", "<cmd>Outline<CR>", desc = "Toggle outline" },
 		},
 		opts = {
 			-- Your setup opts here
